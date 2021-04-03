2ed1b46e917996efbd807a8ef755eee9e34a46a1
Kizito007 <nwakakelechi49@gmail.com>
2021-04-03 13:53:06 +0100

    Setting up header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
+import { Jumbotron, Container } from "react-bootstrap"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Jumbotron fluid>
+        <Container>
+          <h2>City Wide Stores</h2>
+          <p>
+            This is a going to be one of the best stores in the world
+          </p>
+        </Container>
+      </Jumbotron>
+      <Footer />
     </div>
   );
 }
