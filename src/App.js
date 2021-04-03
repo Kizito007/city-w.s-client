374c5144cc4899cc2c80dd21de65f2646d3d8f24
Kizito007 <nwakakelechi49@gmail.com>
2021-04-03 14:03:22 +0100

    restructuring folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css'
 import Header from "./components/layout/Header"
 import Footer from "./components/layout/Footer"
-import { Jumbotron, Container } from "react-bootstrap"
+import Home from "./components/pages/Home"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Jumbotron fluid>
-        <Container>
-          <h2>City Wide Stores</h2>
-          <p>
-            This is a going to be one of the best stores in the world
-          </p>
-        </Container>
-      </Jumbotron>
+      <Home />
       <Footer />
     </div>
   );
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Jumbotron, Container } from "react-bootstrap"
+
+const Home = () => {
+    return (
+        <Jumbotron fluid>
+            <Container>
+            <h2>City Wide Stores</h2>
+            <p>
+                This is a going to be one of the best stores in the world
+            </p>
+            </Container>
+        </Jumbotron>
+    )
+}
+
+export default Home
